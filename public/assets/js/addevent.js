dd48c3c041e27bd68fb1bb025c6964e2690d018e
Greg Allebach <greg.allebach@gmail.com>
2020-08-07 21:27:41 -0400

    set default date and time for add event page

--- public/assets/js/addevent.js
+++ public/assets/js/addevent.js
@@ -1,3 +1,20 @@
+// sets today's date and time as default value
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0');
+var yyyy = today.getFullYear();
+
+// rounds time up to the next hour
+var timeStart = today.getHours() + 1 + ":00";
+// adds to hours to timeStart
+var timeEnd = today.getHours() + 3 + ":00";
+
+today = yyyy + '-' + mm + '-' + dd;
+
+$(".date").attr("value", today);
+$("#eventStartTime").attr("value", timeStart);
+$("#eventEndTime").attr("value", timeEnd);
+
 // create event
 $("#event-btn").on("click", function (event) {
     event.preventDefault();
